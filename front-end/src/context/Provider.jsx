8e3b4ec34675f2178bc93d177841721219773f3c
hsbicalho <hsbicalho@gmail.com>
2022-10-27 18:41:28 -0300

    eslint repair

--- front-end/src/context/Provider.jsx
+++ front-end/src/context/Provider.jsx
@@ -1,14 +1,12 @@
 import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
+
 export const Context = createContext();
 
 export default function Provider({ children }) {
   const [loginFrom, SetLoginForm] = useState({});
 
-  const contextValue = {
-    loginFrom,
-    SetLoginForm,
-  };
+  const contextValue = { loginFrom, SetLoginForm };
 
   return (
     <Context.Provider value={ contextValue }>
