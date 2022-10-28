4e06bfbd4689934d68c49d4d8734bbce920b0b7f
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-28 16:50:22 -0300

    feat: verifica se e-mail existe

--- back-end/src/api/services/user.service.js
+++ back-end/src/api/services/user.service.js
@@ -1,9 +1,12 @@
-const { User } = require('../database/models');
+const { User } = require('../../database/models');
+const CustomError = require('../error/CustomError');
 
 const findByEmail = async ({ email }) => {
   const userExists = await User.findOne({ where: { email } });
-  if (!userExists) throw new Error("Not found")
-  return true;
-}
 
-module.exports = { findByEmail }
+  if (!userExists) throw new CustomError(404, 'Not found');
+  
+  return true;
+};
+
+module.exports = { findByEmail };
