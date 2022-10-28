d56961d04f4f84be1473959ce8187b5f80bb7a68
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-28 16:51:14 -0300

    feat: cria a rota de login

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+require('express-async-errors');
+const errorMiddleware = require('./middlewares/error.middleware');
+const loginRouter = require('./routes/login.router');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/login', loginRouter);
+
+app.use(errorMiddleware);
 
 module.exports = app;
