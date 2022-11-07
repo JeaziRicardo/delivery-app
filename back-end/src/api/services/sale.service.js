bbcb7e779d3bac789fd619e41e522008b531f0e6
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-11-07 18:27:34 -0300

    desenvolvido  service do método getAllSales

--- back-end/src/api/services/sale.service.js
+++ back-end/src/api/services/sale.service.js
@@ -3,12 +3,15 @@ const { validateToken } = require('../helpers/jwtHelper');
 const { getUserByEmail } = require('./user.service');
 
 const create = async (saleObject, token) => {
-  // console.log('DENTRO SERVICE', saleObject);
   const { email } = validateToken(token);
   const userId = await getUserByEmail(email);
   const result = await Sale.create({ ...saleObject, userId });
-  // console.log('RESULT', result.id);
   return result;
 };
 
-module.exports = { create };
+const getAllSales = async () => {
+  const result = await Sale.findAll();
+  return result;
+};
+
+module.exports = { create, getAllSales };
