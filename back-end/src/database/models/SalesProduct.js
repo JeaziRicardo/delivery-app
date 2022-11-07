bab444cfcf38af3215fcc1b2b47e507f464908ba
José Augusto <jaaugusto.n@gmail.com>
2022-11-07 16:04:06 -0300

    renomeia salesProducts model

--- back-end/src/database/models/SalesProduct.js
+++ back-end/src/database/models/SalesProduct.js
@@ -1,15 +1,24 @@
 const SalesProduct = (sequelize, DataTypes) => {
-  const SaleProduct = sequelize.define(
-    "SalesProduct",
-    {
-      saleId: { type: DataTypes.INTEGER, foreignKey: true },
-      productId: { type: DataTypes.INTEGER, foreignKey: true },
-      quantity: { type: DataTypes.INTEGER },
+  const SalesProduct = sequelize.define(
+    "SalesProduct", {
+      saleId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        foreignKey: true,
+      },
+      productId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        foreignKey: true,
+      },
+      quantity: {
+        type: DataTypes.INTEGER
+      },
     },
     { timestamps: false, underscored: true }
   );
 
-  SaleProduct.associate = (models) => {
+  SalesProduct.associate = (models) => {
     models.Sale.belongsToMany(models.Product, {
       as: "sales",
       through: SalesProduct,
