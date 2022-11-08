e23e201d8d4cfbeb531a6eddc7165b7dd397eef4
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-11-08 17:47:05 -0300

    feat: adiciona rota 'customer/orders/:id'

--- back-end/src/api/routes/sale.router.js
+++ back-end/src/api/routes/sale.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const saleController = require('../controllers/sale.controller');
+const salesProductController = require('../controllers/salesProduct.controller');
 
 const saleRouter = Router();
 
 saleRouter.post('/checkout', saleController.create);
-saleRouter.get('/', saleController.getAllSales);
+saleRouter.get('/orders/:id', salesProductController.getSaleByUser);
 saleRouter.get('/:id', saleController.getSaleById);
 saleRouter.get('/products/:id', saleController.getSalesProductsById);
 
